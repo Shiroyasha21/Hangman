665f69a5c1a8a65b29d9ebf1cd8bf1d28794bdd1
Shiroyasha21 <red.ezequiel@gmail.com>
2022-10-13 18:43:48 +0800

    wrote stickman and display features

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -22,27 +22,76 @@ end
 
 # Create the placeholder for the word, the letter count, and guessed remaining
 class Graphics
-  attr_accessor :word, :letter_count, :num_guesses
 
   def initialize(word)
     @word = word
     @word_count = @word.length
-    @display = '%1c ' * @word_count
+    @display = 'Guess: ' + ' ' * 13 + ('%1c ' * @word_count)
     @tiles = Array.new(1) { Array.new(@word_count) { '_' } }
-    display_placeholder
+    @stick_array = [''] * 6
+    @letter_str = ''
+    display_game
   end
 
-  def display_placeholder
+  def stickman_display
     puts "\e[H\e[2J"
+    puts ' ' * 18 + 'Hangman!'
+    puts "
+               |----------
+               |         |
+               |         #{@stick_array[0]}
+               |       #{@stick_array[3]}#{@stick_array[1]}#{@stick_array[2]}
+               |        #{@stick_array[4]} #{@stick_array[5]}
+               |
+               "
+  end
+
+  def display_game
+    stickman_display
     @tiles.each_with_index do |elem, _index|
+      puts ''
       puts(@display % elem)
     end
+    puts ''
+    guessed_letters('')
+  end
+
+  def stickman_progress(guess_count)
+    case guess_count
+    when 1
+      @stick_array[0] = '0'
+    when 3
+      @stick_array[1] = 'I'
+    when 2
+      @stick_array[2] = '-*'
+      @stick_array[3] = '*-'
+    when 4
+      @stick_array[4] = '/'
+    when 5
+      @stick_array[5] = '\\'
+    end
+    display_game
+  end
+
+  def guessed_letters(letter)
+    @letter_str.concat(letter)
+    puts ' ' * 12 + " #{@letter_str}
+    "
   end
 end
 
-Graphics.new('About')
+new = Graphics.new('What')
 
 # Start the game
+class Hangman
+  include Dictionary
+  attr_accessor :letter, :word
+
+  def initialize
+    @letter = letter
+    @word = random_word_gen
+    @game = Graphics.new(@word)
+
 # Generate random word from dictionary
 # Place the word to the placeholder
 
